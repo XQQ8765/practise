f41073b12071426cc8fc23be4cb6e92addc3d1b8
RabbitXiao <rabbit.xiao@quest.com>
2013-11-27 20:09:25 +0800

    commit

--- ImmediateSearch2/extension/background.js
+++ ImmediateSearch2/extension/background.js
@@ -1,8 +1,16 @@
+//Show pageAction when valud url
+//function checkForValidUrl(tabId, changeInfo, tab) {
+//    chrome.pageAction.show(tabId);
+//  }
+//};
+// Listen for any changes to the URL of any tab.
+//chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
 chrome.extension.onMessage.addListener(
 	function(request, sender, sendResponse) {
-		sendResponse({farewell:"Background Received:"+request.greeting});
-		//chrome.tabs.create({
-		//	"http://www.qq.com"
-		//}, function(){console.log('--------------on message function');});
+		sendResponse({farewell:"Background Received:"+request.searchUrl});
+		chrome.tabs.create({
+			url: request.searchUrl
+		}, function(){});
 	}
 );
--- ImmediateSearch2/extension/immediateSearch.js
+++ ImmediateSearch2/extension/immediateSearch.js
@@ -7,18 +7,15 @@ function getSelectedText() {
 }
 function doSearch(searchContent) {
 	var searchUrl='http://www.google.com.hk/#newwindow=1&q='+searchContent+'&safe=strict';
-	alert('--searchUrl='+searchUrl);
+	//alert('--searchUrl='+searchUrl);
 	//alert('chrome create tab: chrome.tabs:'+chrome.tabs);
-	console.log('--------------send message searchUrl:'+searchUrl);
-	chrome.extension.sendMessage({greeting: "http://www.apple.com/"}, function(response){
+	//console.log('---------------send message searchUrl:'+searchUrl);
+	chrome.extension.sendMessage({searchUrl: searchUrl}, function(response){
 		console.log(response.farewell);
 	});
 }
 function performWhenKeyDown() {
-	//alert('alt+s');
-	//var selectedText = document.selection.createRange();
 	var selectedText = getSelectedText();
-	//alert("selectedText:"+selectedText);
 	doSearch(selectedText);
 	return false;
 }
