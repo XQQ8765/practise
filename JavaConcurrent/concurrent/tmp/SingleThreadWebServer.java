defe22bc8de7674a11c0abf1908fc5b1fd585125
RabbitXiao <rabbit.xiao@quest.com>
2014-06-12 19:10:09 +0800

    -a
    c5

---
+++ JavaConcurrent/concurrent/tmp/SingleThreadWebServer.java
@@ -0,0 +1,15 @@
+package rabbit.share.concurrent;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class SingleThreadWebServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (true) {
+            Socket connection = socket.accept();
+            handleRequest(connection);
+        }
+    }
+}
