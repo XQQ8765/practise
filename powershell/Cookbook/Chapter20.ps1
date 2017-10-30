bf5a5a7ef397ba14ce576bd7ddfa3b2c080d1da1
Rabbit Xiao <Rabbit.Xiao@quest.com>
2017-10-30 16:40:02 +0800

    Update Chapter20 for Cookbook

--- powershell/Cookbook/Chapter20.ps1
+++ powershell/Cookbook/Chapter20.ps1
@@ -43,3 +43,139 @@ $compareDate = (Get-Date).AddDays(-30)
 
 $daysTillChristmas = [DateTime] "December 25" - (Get-Date)
 $daysTillChristmas.Days
+
+
+##20.4. Clear the Content of a File
+Clear-Content test.txt
+
+
+##20.5. Manage and Change the Attributes of a File
+#update the ReadOnly, Hidden, or System attributes of a file.
+attrib +r test.txt
+attrib -s test.txt
+
+$file = Get-Item test.txt
+$file.IsReadOnly = $true
+
+$file = Get-Item test.txt
+$file.Attributes = "ReadOnly,NotContentIndexed"
+
+$file.Attributes = "ReadOnly","System","NotContentIndexed"
+$file.Mode
+$file.Attributes
+
+
+##20.6. Find Files That Match a Pattern
+Get-ChildItem *.txt
+Get-ChildItem -Filter *~2*
+Get-ChildItem -Exclude *.txt
+
+Get-ChildItem -Include *.txt -Recurse
+Get-ChildItem *.txt -Recurse
+Get-ChildItem -Path c:\temp\*.txt -Recurse
+
+Get-ChildItem -Filter *.txt -Recurse
+Get-ChildItem -Exclude *.txt -Recurse
+
+Get-ChildItem | Where-Object { $_.Name -match '^KB[0-9]+\.log$' }
+Get-ChildItem -Recurse | Where-Object { $_.DirectoryName -match 'Release' }
+Get-ChildItem -Recurse | Where-Object { $_.FullName -match 'temp' }
+
+
+##20.7. Manage Files That Include Special Characters
+Get-ChildItem -LiteralPath '[My File].txt'
+
+
+##20.8. Program: Get Disk Usage Information
+#Example 20-2. Get-DiskUsage.ps1
+
+
+##20.9. Monitor a File for Changes
+Get-Content log.txt -Wait
+
+
+##20.10. Get the Version of a DLL or Executable
+$file = Get-Item $pshome\powershell.exe
+$file.VersionInfo
+
+Get-ChildItem $env:WINDIR |
+Select -Expand VersionInfo -ErrorAction SilentlyContinue
+
+
+##20.11. Program: Get the MD5 or SHA1 Hash of a File
+#Example 20-3. Get-FileHash.ps1
+dir | Get-FileHash
+
+
+##20.12. Create a Directory
+md NewDirectory
+
+
+##20.13. Remove a File or Directory
+Test-Path NewDirectory
+Remove-Item NewDirectory
+
+
+##20.14. Rename a File or Directory
+Rename-Item example.txt example2.txt
+
+
+##20.15. Move a File or Directory
+Move-Item example.txt c:\temp\example2.txt
+
+
+##20.16. Create and Map PowerShell Drives
+#To create a custom drive, use the New-PSDrive cmdlet:
+$myDocs = [Environment]::GetFolderPath("MyDocuments")
+New-PSDrive -Name MyDocs -Root $myDocs -PSProvider FileSystem
+
+
+##20.17. Access Long File and Directory Names
+#Use the -Persist parameter of the New-PSDrive cmdlet to create a new drive, using the long portion of the path as the root
+PS > $root = "\\server\share\some_long_directory_name"
+PS > New-PSDrive -Name L -Root $root -PSProvider FileSystem -Persist
+PS > Get-Item L:\some_long_file_name.txt
+
+
+##20.18. Unblock a File
+#Use the Unblock-File cmdlet to clear the ¡°Downloaded from the Internet¡± flag on a file
+Unblock-File c:\downloads\file.zip
+Get-ChildItem -Recurse | Unblock-File
+
+
+##20.19. Interact with Alternate Data Streams
+Get-Item .\a.zip -Stream *
+Set-Content .\a.zip:MyCustomStream -Value "Hello World"
+Get-Item .\a.zip -Stream *
+
+
+##20.20. Program: Move or Remove a Locked File
+#Example 20-4. Move-LockedFile.ps1
+
+
+##20.21. Get the ACL of a File or Directory
+#To retrieve the ACL of a file, use the Get-Acl cmdlet:
+Get-Acl example.txt
+#Example 20-5. Get-AclMisconfiguration.ps1
+
+
+##20.22. Set the ACL of a File or Directory
+$acl = Get-Acl example.txt
+$arguments = "LEE-DESK\Guest","FullControl","Deny"
+$accessRule =
+New-Object System.Security.AccessControl.FileSystemAccessRule $arguments
+$acl.SetAccessRule($accessRule)
+$acl | Set-Acl example.txt
+
+
+##20.23. Program: Add Extended File Properties to Files
+#Example 20-6. Add-ExtendedFileProperties.ps1
+
+
+##20.24. Program: Create a Filesystem Hard Link
+#Example 20-7. New-FilesystemHardLink.ps1
+
+
+##20.25. Program: Create a ZIP Archive
+#Example 20-8. New-ZipFile.ps1
+dir *.ps1 | New-ZipFile scripts.zip
