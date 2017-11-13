c25cf9313aaad3988c3c09931add06d6b395a626
Rabbit Xiao <Rabbit.Xiao@quest.com>
2017-11-13 11:18:24 +0800

    [rabbit_hystrix] Add project rabbit_hystrix_hello_service.

---
+++ springboot/rabbit_hystrix/rabbit_hystrix_hello_service/src/main/java/com/rabbit/practise/hystrix/service/HelloConstroller.java
@@ -0,0 +1,26 @@
+package com.rabbit.practise.hystrix.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloConstroller {
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String hello() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:" + instance.getHost() + ", serviceid:" +instance.getServiceId() + ", instance:" + instance);
+        String result = "Hello World. instance: " + instance +", date:" + new Date();
+        return result;
+    }
+}
