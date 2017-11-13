2a8ed290903a654a961d1fe83e26d086811a4def
Rabbit Xiao <Rabbit.Xiao@quest.com>
2017-11-13 11:39:56 +0800

    Add project rabbit_service_consumer.

---
+++ springboot/rabbit_hystrix/rabbit_service_consumer/src/main/java/com/rabbit/practise/hystrix/service/consumer/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.rabbit.practise.hystrix.service.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer", method= RequestMethod.GET)
+    public String helloConsumer() {
+        String serviceResult = restTemplate.getForEntity("http://rabbit-hystrix-hello-service/hello", String.class)
+                .getBody();
+        return "ConsumerController: hello-service result ---> " + serviceResult;
+    }
+}
