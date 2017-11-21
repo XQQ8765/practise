7bef874a2d69d9e54195c4767be0f73bb890f6ce
Rabbit Xiao <Rabbit.Xiao@quest.com>
2017-11-21 15:12:06 +0800

    [rabbit-apigateway] Add AccessFilter for project "rabbit-api-gateway".

--- springboot/rabbit-apigateway/rabbit-api-gateway/src/main/java/com/rabbit/practise/apigateway/App.java
+++ springboot/rabbit-apigateway/rabbit-api-gateway/src/main/java/com/rabbit/practise/apigateway/App.java
@@ -3,6 +3,7 @@ package com.rabbit.practise.apigateway;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -11,5 +12,10 @@ public class App
     public static void main( String[] args )
     {
         new SpringApplicationBuilder(App.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
