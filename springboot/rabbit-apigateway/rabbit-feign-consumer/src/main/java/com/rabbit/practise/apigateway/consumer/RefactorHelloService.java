e55f51b855236b11fa860b6e759576deabe4c673
Rabbit Xiao <Rabbit.Xiao@quest.com>
2017-11-21 13:54:46 +0800

    [rabbit-apigateway] Add back fallback for HelloService.

--- springboot/rabbit-apigateway/rabbit-feign-consumer/src/main/java/com/rabbit/practise/apigateway/consumer/RefactorHelloService.java
+++ springboot/rabbit-apigateway/rabbit-feign-consumer/src/main/java/com/rabbit/practise/apigateway/consumer/RefactorHelloService.java
@@ -3,8 +3,7 @@ package com.rabbit.practise.apigateway.consumer;
 import com.rabbit.practise.apigateway.service.api.HelloService;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-//@FeignClient(name="rabbit-service", fallback = HelloServiceFallback.class)
-@FeignClient(name="rabbit-service", configuration = FullLogConfiguration.class)
+@FeignClient(name="rabbit-service", configuration = FullLogConfiguration.class, fallback = HelloServiceFallback.class)
 public interface RefactorHelloService extends HelloService {
 
 }
